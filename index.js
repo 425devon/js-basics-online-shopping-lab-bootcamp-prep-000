a156070f6dabbe58404e13e5336b665a863b9bd1
Devon Langendoerfer <425devon@gmail.com>
2017-11-16 21:13:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -78,5 +78,7 @@ if(tempCart.length === cart.length){
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if(cardNumber === undefined){
+    console.log('Sorry, we don\'t have a credit card on file for you.')
+  }
 }
