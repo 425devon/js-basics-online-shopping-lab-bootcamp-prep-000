0920df3f2d15ebe58c919d03bce34fab1a65a8e5
Devon Langendoerfer <425devon@gmail.com>
2017-11-16 21:18:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -82,6 +82,6 @@ function placeOrder(cardNumber) {
     console.log('Sorry, we don\'t have a credit card on file for you.')
   }else{
     var totalCost = total(cart);
-    console.log('Your total cost is $'totalCost', which will be charged to the card 'cardNumber'.')
+    console.log(totalCost);
   }
 }
