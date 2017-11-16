14263df1014824cde95933b4bd5efc27edfb4bea
Devon Langendoerfer <425devon@gmail.com>
2017-11-16 21:33:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -83,5 +83,6 @@ function placeOrder(cardNumber) {
   }else{
     var totalCost = total();
     console.log('Your total cost is $' + totalCost + ', which will be charged to the card ' + cardNumber + '.');
+    cart = [];
   }
 }
